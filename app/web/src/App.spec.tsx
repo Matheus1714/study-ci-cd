337791677f91c7fe75137db5664e8d30dc6a45c7
matheus1714 <matheussilvamartins1714@gmail.com>
2024-05-18 23:25:11 -0300

    feat(web): create a front end test to app and add configurations to test

---
+++ app/web/src/App.spec.tsx
@@ -0,0 +1,33 @@
+
+import '@testing-library/jest-dom';
+import { render, waitFor } from '@testing-library/react';
+import { describe, expect, vi, it, beforeEach } from 'vitest';
+import { App } from './App';
+import { getAllUsers } from './api';
+
+vi.mock('./api', () => ({
+    getAllUsers: vi.fn(),
+}));
+
+const mockUsers = [
+    { name: 'John Doe', age: 30, company: 'Company A' },
+    { name: 'Jane Smith', age: 25, company: 'Company B' },
+];
+
+describe('App component', () => {
+    beforeEach(() => {
+        (getAllUsers as jest.Mock).mockResolvedValue(mockUsers);
+    });
+
+    it('should renders user data correctly', async () => {
+        const screen = render(<App />);
+
+        await waitFor(() => {
+            mockUsers.forEach(user => {
+                expect(screen.getByText(user.name)).toBeInTheDocument();
+                expect(screen.getByText(user.age.toString())).toBeInTheDocument();
+                expect(screen.getByText(user.company)).toBeInTheDocument();
+            });
+        });
+    });
+});
